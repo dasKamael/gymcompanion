969995cc799585542ebb6187907a4931742221f9
dasKamael <richard.maasri@hotmail.de>
2021-12-12 17:20:52 +0100

    fetching exercises from database

--- lib/constants/consts.dart
+++ lib/constants/consts.dart
@@ -1,4 +1,5 @@
 class ConstValues {
+  static const String url = '${const String.fromEnvironment('BASE_URL')}/api/v1';
   static const defaultSidePadding = 16.0;
   static const defaultTextFieldBorderRadius = 10.0;
 }
--- lib/screens/plan/create_plan/create_plan_controller.dart
+++ lib/screens/plan/create_plan/create_plan_controller.dart
@@ -1,6 +1,9 @@
 import 'dart:developer';
 
+import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:gymcompanion/constants/consts.dart';
 import 'package:gymcompanion/models/exercise.dart';
 import 'package:gymcompanion/screens/plan/create_plan/create_plan_state.dart';
 
@@ -25,16 +28,20 @@ class CreatePlanController extends StateNotifier<CreatePlanState> {
 
   void initCreatePlanPage() {
     // Get available Exercises
-    state = state.copyWith(exercises: [
-      Exercise(id: 1, name: 'Curl'),
-      Exercise(id: 2, name: 'Lats'),
-      Exercise(id: 3, name: 'Trizeps'),
-      Exercise(id: 4, name: 'Bench press'),
-      Exercise(id: 5, name: 'Curl'),
-      Exercise(id: 6, name: 'Lats'),
-      Exercise(id: 7, name: 'Trizeps'),
-      Exercise(id: 8, name: 'Bench press'),
-    ]);
+    state = state.copyWith(exercises: []);
+    getExercises();
+  }
+
+  Future<void> getExercises() async {
+    final dio = Dio();
+    final url = '${ConstValues.url}/exercise';
+
+    Response response = await dio.get(url);
+    for (final data in response.data['result']) {
+      log(data.toString());
+      state.exercises.add(Exercise.fromJson(data));
+    }
+    state = state.copyWith();
   }
 
   void reorderExerciseList(int oldIndex, int newIndex) {
