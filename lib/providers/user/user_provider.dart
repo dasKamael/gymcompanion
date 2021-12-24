bea01edc4b234f63505593e29643a72c1520aafe
dasKamael <richard.maasri@hotmail.de>
2021-12-24 21:13:25 +0100

    problem removing

--- lib/providers/user/user_provider.dart
+++ lib/providers/user/user_provider.dart
@@ -2,16 +2,19 @@ import 'dart:async';
 
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:gymcompanion/constants/consts.dart';
+import 'package:gymcompanion/providers/providers.dart';
 import 'package:gymcompanion/providers/user/user_state.dart';
 import 'package:gymcompanion/services/auth/auth_repository_impl.dart';
 
 final userProvider = StateNotifierProvider<UserStateProvider, UserState>(
-  (ref) => UserStateProvider(ref.read),
+  (ref) => UserStateProvider(
+    ref.read,
+    () => ref.read(dioProvider.future),
+  ),
 );
 
 class UserStateProvider extends StateNotifier<UserState> {
-  UserStateProvider(this._read)
+  UserStateProvider(this._read, this._dioProvider)
       : super(UserState(
           id: 0,
           firebase_id: '',
@@ -19,12 +22,13 @@ class UserStateProvider extends StateNotifier<UserState> {
         ));
 
   final Reader _read;
+  Future<Dio> Function() _dioProvider;
 
   Future<void> getUser() async {
-    final dio = Dio();
-    final url = '${ConstValues.url}/users';
+    final dio = await _dioProvider();
     final firebaseId = _read(authServiceProvider).getCurrentUser()!.uid;
-    Response result = await dio.get(url, queryParameters: {'firebaseUid': firebaseId});
+
+    Response result = await dio.get('/users', queryParameters: {'firebaseUid': firebaseId});
     Map user = result.data['data'];
     state = state.copyWith(
       id: user['id'],
@@ -34,11 +38,12 @@ class UserStateProvider extends StateNotifier<UserState> {
   }
 
   Future<void> createUser(String userName) async {
-    final dio = Dio();
-    final url = '${ConstValues.url}/users/createUser';
+    final dio = await _dioProvider();
     final firebaseId = _read(authServiceProvider).getCurrentUser()!.uid;
+
     state = state.copyWith(userName: userName);
-    await dio.post(url, queryParameters: {'firebaseUid': firebaseId, 'userName': state.userName});
+    await dio.post('/users/createUser',
+        queryParameters: {'firebaseUid': firebaseId, 'userName': state.userName});
     await getUser();
   }
 
